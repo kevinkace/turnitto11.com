426a2b46eda59b87ad13e6438ce7f9bf9111b491
Kevin Cameron <kevinacameron@hotmail.com>
2016-07-23 00:12:26 -0700

    Video background.

--- app/pages/home.js
+++ app/pages/home.js
@@ -7,6 +7,13 @@ const m = require("mithril"),
 module.exports = {
     view : (ctrl, options) => [
         m(header),
-        m("a[href=/posts]", "CONTINUE")
+        m("a[href=/posts]", "CONTINUE"),
+        m("video", {
+                autoplay : true,
+                loop     : true,
+                class    : "mc4e206468_videobg"
+            },
+                m("source", { src : "/vid/turn11.mp4" })
+            )
     ]
 };
